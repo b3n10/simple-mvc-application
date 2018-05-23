801c510ea39c2fb2ac888bdaf16973d1cd19cbe8
b3n10 <francisben.lleve@gmail.com>
2018-05-23 22:57:58 +0800

    Require autoload and database

---
+++ app/database.php
@@ -0,0 +1,5 @@
+<?php
+
+// functionality for db
+
+
--- app/init.php
+++ app/init.php
@@ -1,4 +1,10 @@
 <?php
 
+// require autoloader
+require_once '../vendor/autoload.php';
+
+// require db connection
+require_once 'database.php';
+
 require_once 'core/App.php';
 require_once 'core/Controller.php';
