0742ab97c2b98d604faba2b686deee516b59e32a
b3n10 <francisben.lleve@gmail.com>
2018-05-23 23:44:54 +0800

    Fix comments

--- app/models/User.php
+++ app/models/User.php
@@ -5,8 +5,10 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class User extends Eloquent {
 	public	$name,
-					// to choose no timestamp on db
-					// Eloquent automatically fills timestamps (created_at and updated_at) if cols are present on database table
+					// when managing db (e.g. insert, update, etc.)
+					// Eloquent automatically fills timestamps (created_at and updated_at)
+					// if cols are present on database table
+					// to choose no timestamp on db, add this:
 					$timestamps = [];
 
 	// only include fields (cols) needed to manage on db
