f26c7f47064c373c70096f2c71ff0b493912b009
b3n10 <francisben.lleve@gmail.com>
2018-05-23 23:43:09 +0800

    Fix $timestamp to $timestamps

--- app/models/User.php
+++ app/models/User.php
@@ -7,7 +7,7 @@ class User extends Eloquent {
 	public	$name,
 					// to choose no timestamp on db
 					// Eloquent automatically fills timestamps (created_at and updated_at) if cols are present on database table
-					$timestamp = [];
+					$timestamps = [];
 
 	// only include fields (cols) needed to manage on db
 	// and to prevent MassAssignmentException error
