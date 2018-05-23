674657f5278ff6c9f325135ebff380fd4d93d468
b3n10 <francisben.lleve@gmail.com>
2018-05-23 23:31:31 +0800

    Add $timestamp and $fillable properties

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,15 @@
 <?php
 // handles logic in the Model (MVC)
 
-class User {
-	public $name;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent {
+	public	$name,
+					// to choose no timestamp on db
+					// Eloquent automatically fills timestamps (created_at and updated_at) if cols are present on database table
+					$timestamp = [];
+
+	// only include fields (cols) needed to manage on db
+	// and to prevent MassAssignmentException error
+	protected $fillable = ['username', 'email'];
 }
