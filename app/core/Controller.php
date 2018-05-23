c14185da2ac90e0927a032bd4519c290fafa2e12
b3n10 <francisben.lleve@gmail.com>
2018-05-23 20:00:58 +0800

    Add comments

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,8 +3,13 @@
 class Controller {
 
 	public function model($model) {
+
+		// require $model
 		require_once '../app/models/' . $model . '.php';
+
+		// return instance of $model
 		return new $model();
+
 	}
 
 }
