9b563e5eb3adcdea08e33409e8a4ff96277b2fbe
b3n10 <francisben.lleve@gmail.com>
2018-05-23 23:14:11 +0800

    Create db connection

--- app/database.php
+++ app/database.php
@@ -5,3 +5,15 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule();
 // or
 // $capsule = new Illuminate\Database\Capsule\Manager\Capsule();
+
+// create connection
+$capsule->addConnection([
+	'driver'			=> 'mysql',
+	'host'				=> '127.0.0.1',
+	'username'		=> 'root',
+	'password'		=> 'jairah',
+	'database'		=> 'simplemvc',
+	'charset'			=> 'utf8',
+	'collation'		=> 'utf8_unicode_ci',
+	'prefix'			=> ''
+]);
