db279c0354ccaba028031a63ec34f883e4896bfe
b3n10 <francisben.lleve@gmail.com>
2018-05-23 16:45:10 +0800

    Add parseUrl method

--- app/core/App.php
+++ app/core/App.php
@@ -8,4 +8,17 @@ class App {
 
 	public function __construct() {
 	}
+
+	public function parseUrl() {
+
+		if (isset($_GET['url'])) {
+
+			// remove the trailing slash
+			// sanitize the url with filter_var
+			// explode to an array by slash
+			return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+		}
+
+	}
+
 }
